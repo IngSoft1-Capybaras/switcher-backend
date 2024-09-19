cc4977ff759cdaa23376c52a065cfc33ba08b415
Facundo Sharry <facundo.sharry@gmail.com>
2024-09-18 23:31:16 -0300

    SCRUM-6 upd model to match acceptance criteria

--- player/models.py
+++ player/models.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from pony.orm import Required, PrimaryKey, Set
+from pony.orm import Required, PrimaryKey, Set, Optional
 from game.models import Game
 from gameState.models import GameState
 from figureCards.models import FigureCard
@@ -7,7 +7,7 @@ from movementCards.models import MovementCard
 from database.db import db
 
 # enum de los turnos
-class turnEnum(Enum):
+class turnEnum(str,Enum):
     PRIMERO = "primero"
     SEGUNDO = "segundo"
     TERCERO = "tercero"
@@ -15,10 +15,14 @@ class turnEnum(Enum):
 
 # modelo de jugador
 class Player(db.Entity):
-    id = PrimaryKey(int)
+    id = PrimaryKey(int, auto=True)
     name = Required(str)
-    turn = Required(turnEnum)
-    idGame = Required(Game)
+    turn = Optional(turnEnum, nullable=True) 
+    host = Required(bool)
+    idGame = Required(Game)         
     idGameStatus = Required(GameState)
     figureCards = Set(FigureCard)
     movementCards = Set(MovementCard)
+
+    class Config:
+        from_attributes = True
