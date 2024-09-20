1b8556f4af19f72de4c93f30543a08cf6a797bd4
Ignacio Gomez <57643368+NachoGz@users.noreply.github.com>
2024-09-19 22:37:10 -0300

    SCRUM-15 renamed FigureCard to FigureCardSchema

--- player/schemas.py
+++ player/schemas.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from pydantic import BaseModel
 from typing import List, Optional
-from figureCards.schemas import FigureCard
+from figureCards.schemas import FigureCardSchema
 from movementCards.schemas import MovementCard
 
 # schema del enum de turnos
