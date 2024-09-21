8b74d0b02a5c4b9f5f7a1026aacdd340374ca385
Ignacio Gomez <57643368+NachoGz@users.noreply.github.com>
2024-09-20 22:01:50 -0300

    fixed dependancy injection

--- figureCards/endpoints.py
+++ figureCards/endpoints.py
@@ -1,8 +1,5 @@
-from fastapi import APIRouter, status, HTTPException, Depends
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from .models import FigureCard
-from player.models import Player
-from game.models import Game
 from database.db import get_db
 from .figure_cards_repository import FigureCardsRepository
 
@@ -10,12 +7,12 @@ figure_cards_router = APIRouter()
 
 # Obtener todas las cartas de figura
 @figure_cards_router.get("/deck/figure/{game_id}/{player_id}")
-async def get_figure_cards(player_id: int, game_id: int, db: Session = Depends(get_db), repo: FigureCardsRepository() = Depends()):
-    return repo.get_figure_cards(player_id, game_id, db)
+async def get_figure_cards(game_id: int, player_id: int, db: Session = Depends(get_db), repo: FigureCardsRepository = Depends()):
+    return repo.get_figure_cards(game_id, player_id, db)
 
 
 # Obtener una carta de figura especifica
 # Obtener todas las cartas de figura
 @figure_cards_router.get("/deck/figure/{game_id}/{player_id}/{card_id}")
-async def get_figure_cards(player_id: int, game_id: int, card_id: int, db: Session = Depends(get_db), repo: FigureCardsRepository() = Depends()):
-    return repo.get_figure_cards_by_id(player_id, game_id, card_id, db)
+async def get_figure_cards(game_id: int, player_id: int, card_id: int, db: Session = Depends(get_db), repo: FigureCardsRepository = Depends()):
+    return repo.get_figure_cards_by_id(game_id, player_id, card_id, db)
