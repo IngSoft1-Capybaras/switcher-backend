ae70d929b85af779013ccec27ba6a2c78c58a4c1
Ignacio Gomez <57643368+NachoGz@users.noreply.github.com>
2024-09-22 17:40:36 -0300

    fix comment

--- movementCards/endpoints.py
+++ movementCards/endpoints.py
@@ -5,13 +5,12 @@ from .movement_cards_repository import MovementCardsRepository
 
 movement_cards_router = APIRouter()
 
-# Obtener cartas de movement
+# Obtener cartas de movimiento
 @movement_cards_router.get("/deck/movement/{game_id}/{player_id}")
 async def get_movement_cards(game_id: int, player_id: int, db: Session = Depends(get_db), repo: MovementCardsRepository = Depends()):
     return repo.get_movement_cards(game_id, player_id, db)
 
-# Obtener una carta de figura especifica
-# Obtener todas las cartas de movement
+# Obtener todas las cartas de movimiento
 @movement_cards_router.get("/deck/movement/{game_id}/{player_id}/{card_id}")
 async def get_movement_cards(game_id: int, player_id: int, card_id: int, db: Session = Depends(get_db), repo: MovementCardsRepository = Depends()):
     return repo.get_movement_cards_by_id(game_id, player_id, card_id, db)
