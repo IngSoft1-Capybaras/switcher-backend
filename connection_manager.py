3a2d27be623eba7b5c1463770c0fd429c97fa99d
rxciog <rocio.guadalupe.gomez@mi.unc.edu.ar>
2024-09-26 18:30:12 -0300

    SCRUM-31 modificar connection manager para el nuevo endpoint

--- connection_manager.py
+++ connection_manager.py
@@ -1,19 +1,28 @@
+from typing import Dict, List
 from fastapi import WebSocket
 
 class ConnectionManager:
     def __init__(self):
         self.active_connections: list[WebSocket] = []
+        self.game_connections: Dict[int, List[WebSocket]] = {}
 
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
         self.active_connections.append(websocket)
 
+    async def connect_game(self, websocket: WebSocket, game_id: int):
+        await websocket.accept()
+        self.game_connections[game_id].append(websocket)
+
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
 
-
     async def broadcast(self, message: dict):
         for connection in self.active_connections:
+            await connection.send_json(message)
+            
+    async def broadcast_game(self, game_id: int, message: dict):
+        for connection in self.game_connections[game_id]:
             await connection.send_json(message)
 
 manager = ConnectionManager()
--- main.py
+++ main.py
@@ -56,6 +56,16 @@ async def websocket_endpoint(websocket: WebSocket):
     except WebSocketDisconnect:
         manager.disconnect(websocket)
 
+@app.websocket("/ws/{game_id}")
+async def websocket_endpoint(websocket: WebSocket, game_id):
+    await manager.connect_game(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.broadcast(data)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+
 
 # Create the database tables
 Base.metadata.create_all(bind=engine)
