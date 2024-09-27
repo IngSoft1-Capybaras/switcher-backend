25bdc4ecaf3c2927d0fd1367ad75d4bc054db6df
rxciog <rocio.guadalupe.gomez@mi.unc.edu.ar>
2024-09-27 17:46:57 -0300

    SCRUM-31 añadir endpoint

--- player/endpoints.py
+++ player/endpoints.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from database.db import get_db
 from .player_repository import PlayerRepository
+from .schemas import PlayerJoinRequest
+from game.game_repository import GameRepository
 from connection_manager import manager
 
 player_router = APIRouter()
@@ -27,3 +29,21 @@ async def get_player_by_id(game_id: int, player_id: int, db: Session = Depends(g
     await manager.broadcast(message)
 
     return message
+
+@player_router.post("/players/join/{game_id}", status_code= status.HTTP_201_CREATED)
+async def join_game(game_id: int, player_name: PlayerJoinRequest, db: Session = Depends(get_db), repo: PlayerRepository = Depends(), game_repo: GameRepository = Depends()):
+    #Verificar que no se supere el maximo de jugadores
+    game = game_repo.get_game_by_id(game_id, db)
+    players_in_game = game_repo.count_players_in_game(game_id, db)
+    
+    if game.maxPlayers == players_in_game:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="The game is full.")
+    
+    player_id = repo.create_player(game_id, player_name.player_name, db)
+    
+    player_list_update = {
+            "type":f"{game_id}:GAME_INFO_UPDATE"
+        }
+    await manager.broadcast(player_list_update)
+        
+    return player_id
