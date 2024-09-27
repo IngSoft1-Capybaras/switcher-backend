ee96421d45641da4feb7387ccef5650b3d4e93f7
Ignacio Gomez <57643368+NachoGz@users.noreply.github.com>
2024-09-27 16:03:22 -0300

    made session an argument and added rollback transaction to each test

--- tests/test_board_repository_integration.py
+++ tests/test_board_repository_integration.py
@@ -19,69 +19,72 @@ Session = sessionmaker(bind=engine)
 def board_repository():
     return BoardRepository()
 
+
 @pytest.mark.integration_test
-def test_create_new_board(board_repository: BoardRepository):
-    session = Session()
+def test_create_new_board(board_repository: BoardRepository, session):
+    # session = Session()
     
-    try:
-        N_boards = session.query(Board).filter(Board.game_id == 1).count()
-    finally:
-        session.close()
+    # try:
+    N_boards = session.query(Board).filter(Board.game_id == 1).count()
+    # finally:
+    #     session.close()
     
     board_repository.create_new_board(1,session)
     
-    session = Session()
+    # session = Session()
     
-    try:
-        assert session.query(Board).filter(Board.game_id == 1).count() == N_boards + 1
-    finally:
-        session.close()
+    # try:
+    assert session.query(Board).filter(Board.game_id == 1).count() == N_boards + 1
+    # finally:
+    #     session.close()
         
-@pytest.mark.integration_test
-def test_get_board(board_repository: BoardRepository):
-    session = Session()
-    
-    try:
-        board = board_repository.get_existing_board(1, session)
-        assert board is not None
-    finally:
-        session.close()
 
 @pytest.mark.integration_test
-def test_add_box_to_board(board_repository: BoardRepository):
-    session = Session()
+def test_get_board(board_repository: BoardRepository, session):
+    # session = Session()
     
-    try:
-        N_boxes = session.query(Box).filter(Box.board_id == 1).count()
-    finally:
-        session.close()
-    
-    board_repository.add_box_to_board(1,1,ColorEnum.YELLOW, 4, 3, session)
-    
-    session = Session()
-    
-    try:
-        assert session.query(Box).filter(Box.board_id == 1).count() == N_boxes + 1
-    finally:
-        session.close()
-        
+    # try:
+    board = board_repository.get_existing_board(1, session)
+    assert board is not None
+    # finally:
+    #     session.close()
+
+
 @pytest.mark.integration_test
-def test_configure_board(board_repository: BoardRepository):
-    session = Session()
+def test_add_box_to_board(board_repository: BoardRepository, session):
+    # session = Session()
     
-    try:
-        N_boxes = session.query(Box).filter(Box.game_id == 4).count()
-    finally:
-        session.close()
+    # try:
+    N_boxes = session.query(Box).filter(Box.board_id == 1).count()
+    # finally:
+    #     session.close()
     
-    board_repository.configure_board(4, session)
+    board_repository.add_box_to_board(1, 1, ColorEnum.YELLOW, 4, 3, session)
     
-    session = Session()
+    # session = Session()
     
-    try:
-        assert session.query(Box).filter(Box.game_id == 4).count() == N_boxes + 36
-    finally:
-        session.close()
+    # try:
+    assert session.query(Box).filter(Box.board_id == 1).count() == N_boxes + 1
+    # finally:
+    #     session.close()
+        
+
+@pytest.mark.integration_test
+def test_configure_board(board_repository: BoardRepository, session):
+    # session = Session()
+    
+    # try:
+    N_boxes = session.query(Box).filter(Box.game_id == 99).count()
+    # finally:
+    #     session.close()
+    board_repository.configure_board(99, session)
+    
+    # session = Session()
+    
+    # try:
+    assert session.query(Box).filter(Box.game_id == 99).count() == N_boxes + 36
+    # finally:
+    #     session.close()
 
 
 
--- tests/test_movement_cards_repository.py
+++ tests/test_movement_cards_repository.py
@@ -1,5 +1,6 @@
 import pytest
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import NoResultFound
 from movementCards.movement_cards_repository import MovementCardsRepository
 from board.models import  Board, Box
 from figureCards.models import FigureCard
@@ -18,20 +19,54 @@ Session = sessionmaker(bind=engine)
 def movement_card_repository():
     return MovementCardsRepository()
 
-@pytest.mark.integration_test
-def test_create_new_movement_card(movement_card_repository: MovementCardsRepository):
+
+# @pytest.mark.integration_test
+def test_get_movement_cards(movement_cards_repository: MovementCardsRepository, session):
+    # session = Session()
+    # try:
+    N_cards = session.query(MovementCard).filter(MovementCard.game_id == 1, 
+                                                MovementCard.player_id == 1).count()
+
+    list_of_cards = movement_cards_repository.get_movement_cards(1, 1, session)
+    
+    assert len(list_of_cards) == N_cards
+
+    # finally:
+    #     session.close()
+
+
+# @pytest.mark.integration_test
+def test_get_movement_card_by_id(movement_cards_repository: MovementCardsRepository, session):
+    # session = Session()
+    try:
+        # busco la cantidad de cartas con todos id 1
+        test_card = session.query(MovementCard).filter(MovementCard.game_id == 1,
+                                                  MovementCard.player_id == 1,
+                                                  MovementCard.id == 1).one()
+        
+        movement_card = movement_cards_repository.get_movement_card_by_id(1, 1, 1, session)
+
+        assert test_card.id == movement_card.id
+    except NoResultFound:
+        raise ValueError("There is no movement card with game_id=1, player_id=1 and id=1")
+    # finally:
+    #     session.close()
+
+
+# @pytest.mark.integration_test
+def test_create_new_movement_card(movement_card_repository: MovementCardsRepository, session):
+    # session = Session()
+    
+    # try:
+    N_cards = session.query(MovementCard).filter(MovementCard.game_id == 1, 
+                                                     MovementCard.player_id == 17).count()
+    # finally:
+    #     session.close()
+    
+    movement_card_repository.create_movement_card(17, 1, typeEnum.EN_L_DER, session)
     session = Session()
     
-    try:
-        N_cards = session.query(MovementCard).filter(MovementCard.game_id == 1).count()
-    finally:
-        session.close()
-    
-    movement_card_repository.create_movement_card(1, typeEnum.EN_L_DER, session)
-    
-    session = Session()
-    
-    try:
-        assert session.query(MovementCard).filter(MovementCard.game_id == 1).count() == N_cards + 1
-    finally:
-        session.close()
+    # try:
+    assert session.query(MovementCard).filter(MovementCard.game_id == 1).count() == N_cards + 1
+    # finally:
+    #     session.close()
