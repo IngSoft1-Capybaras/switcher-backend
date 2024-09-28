8f9b293a7b5496c2e72c0642136df7772b2ea096
Ignacio Gomez <57643368+NachoGz@users.noreply.github.com>
2024-09-28 17:18:02 -0300

    changed some tests and added comments

--- tests/test_board_repository_integration.py
+++ tests/test_board_repository_integration.py
@@ -32,101 +32,48 @@ def game_repository():
 
 @pytest.mark.integration_test
 def test_create_new_board(board_repository: BoardRepository, session):
-    # session = Session()
-    
-    # try:
     N_boards = session.query(Board).filter(Board.game_id == 1).count()
-    # finally:
-    #     session.close()
     
-    board_repository.create_new_board(1,session)
+    board_repository.create_new_board(1, session)
     
-    # session = Session()
-    
-    # try:
     assert session.query(Board).filter(Board.game_id == 1).count() == N_boards + 1
-    # finally:
-    #     session.close()
         
 
 @pytest.mark.integration_test
 def test_get_board(board_repository: BoardRepository, session):
-    # session = Session()
-    
-    # try:
     board = board_repository.get_existing_board(1, session)
-    assert board is not None
-    # finally:
-    #     session.close()
 
-
-# @pytest.mark.integration_test
-# def test_add_box_to_board(board_repository: BoardRepository, session):
-#     # session = Session()
-    
-#     # try:
-#     N_boxes = session.query(Box).filter(Box.board_id == 1).count()
-#     # finally:
-#     #     session.close()
-    
-#     board_repository.add_box_to_board(1, 1, ColorEnum.YELLOW, 4, 3, session)
-    
-#     # session = Session()
-    
-#     # try:
-#     assert session.query(Box).filter(Box.board_id == 1).count() == N_boxes + 1
-#     # finally:
-#     #     session.close()
+    assert isinstance(board, BoardOut)
+    assert board.game_id == 1
         
 
 @pytest.mark.integration_test
-def test_configure_board(board_repository: BoardRepository, session):
-    # session = Session()
-    
-    # try:
-    N_boxes = session.query(Box).filter(Box.game_id == 99).count()
-    # finally:
-    #     session.close()
-    board_repository.configure_board(99, session)
-    
-    # session = Session()
-    
-    # try:
-    assert session.query(Box).filter(Box.game_id == 99).count() == N_boxes + 36
-    # finally:
-    #     session.close()
-
-
-
-@pytest.mark.integration_test
-def test_get_existing_board_for_populated_game(board_repository: BoardRepository, session):
-    game = session.query(Game).first()
-    
-    result = board_repository.get_existing_board(game.id, session)
-    
-    assert isinstance(result, BoardOut)
-    assert result.game_id == game.id
+def test_configure_board(board_repository: BoardRepository, session):    
+    N_boxes = session.query(Box).filter(Box.game_id == 2).count()
+    board_repository.configure_board(2, session)
+        
+    assert session.query(Box).filter(Box.game_id == 2).count() == N_boxes + 36   
 
 
 @pytest.mark.integration_test
 def test_create_new_board_for_existing_game(game_repository: GameRepository, board_repository: BoardRepository, session):
-    # Create a new game without a board
+    # creo un nuevo juego sin tablero
     res = game_repository.create_game(GameCreate(name="Test Game 2", max_players=4, min_players=2),
                                 PlayerCreateMatch(name="Test Player"),
                                 session)
     new_game = res.get('game')
 
+    # le creo un tablero al juego
     board = board_repository.create_new_board(new_game.id, session)
     
-    assert board is not None
+    assert isinstance(board, BoardOut)
     assert board.game_id == new_game.id
 
 
 @pytest.mark.integration_test
 def test_add_box_to_existing_board(board_repository: BoardRepository, session):
-    # Get an existing board
+    # busco un tablero que exista
     existing_board = session.query(Board).first()
-    print(f"id de board {existing_board.id}")
 
     initial_box_count = session.query(Box).filter(Box.board_id == existing_board.id).count()
     
@@ -138,13 +85,13 @@ def test_add_box_to_existing_board(board_repository: BoardRepository, session):
 
 @pytest.mark.integration_test
 def test_configure_board_for_new_game(game_repository: GameRepository, board_repository: BoardRepository, session):
-    # Create a new game without a board
+    # creo un nuevo juego sin tablero
     res = game_repository.create_game(GameCreate(name="Another test game", max_players=4, min_players=2),
                                 PlayerCreateMatch(name="Test Player"),
                                 session)
     new_game = res.get('game')
 
-    
+    # configuro un tablero para el juego sin haber creado un tablero antes
     result = board_repository.configure_board(new_game.id, session)
     
     assert result == {"message": "Board created successfully"}
@@ -158,7 +105,7 @@ def test_configure_board_for_new_game(game_repository: GameRepository, board_rep
 
 @pytest.mark.integration_test
 def test_configure_board_color_distribution(game_repository: GameRepository, board_repository: BoardRepository, session):
-    # Create a new game without a board
+    # creo un juego sin un tablero
     res = game_repository.create_game(GameCreate(name="Color Distribution Test Game", max_players=4, min_players=2),
                             PlayerCreateMatch(name="Test Player"),
                             session)
@@ -169,6 +116,7 @@ def test_configure_board_color_distribution(game_repository: GameRepository, boa
     new_board = session.query(Board).filter(Board.game_id == new_game.id).first()
     boxes = session.query(Box).filter(Box.board_id == new_board.id).all()
     
+    # cuento que cada color aparezca 9 veces
     color_counts = {color: 0 for color in ColorEnum}
     for box in boxes:
         color_counts[box.color] += 1
@@ -178,7 +126,7 @@ def test_configure_board_color_distribution(game_repository: GameRepository, boa
 
 @pytest.mark.integration_test
 def test_configure_board_already_configured(board_repository: BoardRepository, session):
-    # Get an existing game that already has a board
+    # busco un juego que tenga tablero
     existing_game = session.query(Game).join(Board).first()
     
     with pytest.raises(HTTPException) as exc_info:
