a19c816d1eabaf3bb613389027525f1f3c45dedb
Ignacio Gomez <57643368+NachoGz@users.noreply.github.com>
2024-09-29 12:06:45 -0300

    fixed figure utils test

--- tests/test_figure_cards_utils.py
+++ tests/test_figure_cards_utils.py
@@ -31,29 +31,45 @@ def test_create_fig_deck(fig_cards_utils, player_utils):
     assert response == {"message": "Figure deck created"}
     
     expected_calls = [
-        ((1, game_id, typeEnum.TYPE_4, mock_session),),
-        ((1, game_id, typeEnum.TYPE_4, mock_session),),
-        ((1, game_id, typeEnum.TYPE_4, mock_session),),
-        ((1, game_id, typeEnum.TYPE_5, mock_session),),
-        ((1, game_id, typeEnum.TYPE_5, mock_session),),
-        ((1, game_id, typeEnum.TYPE_5, mock_session),),
-        ((1, game_id, typeEnum.TYPE_5, mock_session),),
-        ((1, game_id, typeEnum.TYPE_6, mock_session),),
-        ((1, game_id, typeEnum.TYPE_6, mock_session),),
-        ((1, game_id, typeEnum.TYPE_6, mock_session),),
-        ((1, game_id, typeEnum.TYPE_1, mock_session),),
-        ((1, game_id, typeEnum.TYPE_1, mock_session),),
-        ((1, game_id, typeEnum.TYPE_1, mock_session),),
-        ((1, game_id, typeEnum.TYPE_1, mock_session),),
-        ((1, game_id, typeEnum.TYPE_1, mock_session),),
-        ((1, game_id, typeEnum.TYPE_2, mock_session),),
-        ((1, game_id, typeEnum.TYPE_2, mock_session),),
-        ((1, game_id, typeEnum.TYPE_2, mock_session),),
-        ((1, game_id, typeEnum.TYPE_2, mock_session),),
-        ((1, game_id, typeEnum.TYPE_3, mock_session),),
-        ((1, game_id, typeEnum.TYPE_3, mock_session),),
-        ((1, game_id, typeEnum.TYPE_3, mock_session),),
-        ((1, game_id, typeEnum.TYPE_3, mock_session),)      
+        ((1, game_id, typeEnum.TYPE_4, False, mock_session),),
+        ((1, game_id, typeEnum.TYPE_4, False, mock_session),),
+        ((1, game_id, typeEnum.TYPE_4, False, mock_session),),
+        ((1, game_id, typeEnum.TYPE_5, True, mock_session),),
+        ((1, game_id, typeEnum.TYPE_5, False, mock_session),),
+        ((1, game_id, typeEnum.TYPE_5, False, mock_session),),
+        ((1, game_id, typeEnum.TYPE_5, False, mock_session),),
+        ((1, game_id, typeEnum.TYPE_6, True, mock_session),),
+        ((1, game_id, typeEnum.TYPE_6, True, mock_session),),
+        ((1, game_id, typeEnum.TYPE_6, True, mock_session),),
+        ((1, game_id, typeEnum.TYPE_1, False, mock_session),),
+        ((1, game_id, typeEnum.TYPE_1, False, mock_session),),
+        ((1, game_id, typeEnum.TYPE_1, False, mock_session),),
+        ((1, game_id, typeEnum.TYPE_1, False, mock_session),),
+        ((1, game_id, typeEnum.TYPE_1, False, mock_session),),
+        ((1, game_id, typeEnum.TYPE_2, False, mock_session),),
+        ((1, game_id, typeEnum.TYPE_2, False, mock_session),),
+        ((1, game_id, typeEnum.TYPE_2, False, mock_session),),
+        ((1, game_id, typeEnum.TYPE_2, False, mock_session),),
+        ((1, game_id, typeEnum.TYPE_3, False, mock_session),),
+        ((1, game_id, typeEnum.TYPE_3, False, mock_session),),
+        ((1, game_id, typeEnum.TYPE_3, False, mock_session),),
+        ((1, game_id, typeEnum.TYPE_3, False, mock_session),)
     ]
     
-    fig_cards_utils.fig_card_repo.create_figure_card.assert_has_calls(expected_calls, any_order=True)
+    # fig_cards_utils.fig_card_repo.create_figure_card.assert_has_calls(expected_calls, any_order=True)
+    # me fijo que tenga 23 llamadas
+    assert fig_cards_utils.fig_card_repo.create_figure_card.call_count == len(expected_calls)
+    
+    calls = fig_cards_utils.fig_card_repo.create_figure_card.call_args_list
+    
+    # me fijo que solo hayan 3 cartas mostradas
+    shown_cards = sum(1 for call in calls if call[0][3] is True)
+    assert shown_cards == 3
+
+    # me fijo que las funciones se llamen correctamente
+    for call in calls:
+        assert len(call[0]) == 5 # cada llamada tiene 5 arguments
+        assert isinstance(call[0][0], int)
+        assert isinstance(call[0][1], int)
+        assert isinstance(call[0][2], typeEnum)
+        assert isinstance(call[0][3], bool)
