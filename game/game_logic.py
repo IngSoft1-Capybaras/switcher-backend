6c131555e646756a497b13e48c38ef57862ff3f3
Ignacio Gomez <57643368+NachoGz@users.noreply.github.com>
2024-10-05 17:39:59 -0300

    moved count_players_in_game to repo and added injection dependancy for player_repo

--- game/game_logic.py
+++ game/game_logic.py
@@ -6,6 +6,7 @@ from game.game_repository import GameRepository
 from gameState.game_state_repository import GameStateRepository
 from gameState.models import StateEnum
 from player.models import Player
+from player.player_repository import PlayerRepository
 from connection_manager import manager
 
 
@@ -15,16 +16,6 @@ def get_game_logic(game_repo: GameRepository = Depends()):
 class GameLogic:
     def __init__(self, game_repository: GameRepository):
         self.game_repository = game_repository
-
-    def count_players_in_game(self, game_id: int, db: Session) -> int:
-        try:
-            game = db.query(Game).filter(Game.id == game_id).one()
-            
-        except NoResultFound:
-            raise HTTPException(status_code = 404, detail = "Game not found")
-        
-        player_count = game.players_count()
-        return player_count
 
 
     async def check_win_condition(self, game: Game, db: Session):
@@ -40,16 +31,14 @@ class GameLogic:
         # aca irian el resto de las condiciones que se veran en otros sprints
 
 
-    async def handle_win(self, game_id: int, last_player: Player, db: Session):
+    async def handle_win(self, game_id: int, last_player: Player, db: Session, player_repo: PlayerRepository = Depends()):
         game_state_repository = GameStateRepository()
 
         # actualizo partida a finalizada
         game_state_repository.update_game_state(game_id, StateEnum.FINISHED, db)
         
         # asigno al ultimo jugador como ganador
-        last_player.winner = True
-        db.add(last_player)
-        db.commit()
+        player_repo.assign_winner_of_game(last_player, db)
         
         player_update = {
                 "type": "PLAYER_WINNER",
