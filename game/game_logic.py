2127b30dd0cbae2431d825b3fe1914cdaa4d34f9
rxciog <rocio.guadalupe.gomez@mi.unc.edu.ar>
2024-10-06 10:51:14 -0300

    SCRUM-87 Agregar test handle win

--- game/game_logic.py
+++ game/game_logic.py
@@ -10,12 +10,14 @@ from player.player_repository import PlayerRepository
 from connection_manager import manager
 
 
-def get_game_logic(game_repo: GameRepository = Depends()):
-    return GameLogic(game_repo)
+def get_game_logic(game_repo: GameRepository = Depends(), game_state_repo : GameStateRepository = Depends(), player_repo : PlayerRepository = Depends()):
+    return GameLogic(game_repo,game_state_repo, player_repo)
 
 class GameLogic:
-    def __init__(self, game_repository: GameRepository):
+    def __init__(self, game_repository: GameRepository, game_state_repository: GameStateRepository, player_repository: PlayerRepository):
         self.game_repository = game_repository
+        self.game_state_repo = game_state_repository
+        self.player_repo = player_repository
 
 
     async def check_win_condition(self, game: Game, db: Session):
@@ -32,14 +34,12 @@ class GameLogic:
 
 
     async def handle_win(self, game_id: int, last_player: Player, db: Session):
-        game_state_repository = GameStateRepository()
-        player_repo = PlayerRepository()
 
         # actualizo partida a finalizada
-        game_state_repository.update_game_state(game_id, StateEnum.FINISHED, db)
+        self.game_state_repo.update_game_state(game_id, StateEnum.FINISHED, db)
         
         # asigno al ultimo jugador como ganador
-        player_repo.assign_winner_of_game(last_player, db)
+        self.player_repo.assign_winner_of_game(last_player, db)
         
         player_update = {
                 "type": "PLAYER_WINNER",
