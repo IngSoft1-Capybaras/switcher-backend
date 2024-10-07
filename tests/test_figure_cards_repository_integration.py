49020a9d9d892e2c3d486123d103253da02fd2c5
rxciog <rocio.guadalupe.gomez@mi.unc.edu.ar>
2024-10-07 15:02:16 -0300

    SCRUM-94 agregar tests necesarios

--- tests/test_figure_cards_repository_integration.py
+++ tests/test_figure_cards_repository_integration.py
@@ -8,6 +8,7 @@ from game.models import Game
 from gameState.models import GameState, StateEnum
 from movementCards.models import MovementCard
 from player.models import Player
+from fastapi import HTTPException
 
 
 from database.db import engine
@@ -74,8 +75,8 @@ def test_grab_figure_cards(figure_cards_repository, session):
     session.add(game_state)
     session.commit()
     
-    player1 = Player(id=18, name="Player1", game_id=game.id, game_state_id=game_state.id, host=True, winner=False)
-    player2 = Player(id=19, name="Player2", game_id=game.id, game_state_id=game_state.id, host=False, winner=False)
+    player1 = Player(name="Player1", game_id=game.id, game_state_id=game_state.id, host=True, winner=False)
+    player2 = Player(name="Player2", game_id=game.id, game_state_id=game_state.id, host=False, winner=False)
     session.add_all([player1, player2])
     session.commit()
     
@@ -101,3 +102,77 @@ def test_grab_figure_cards(figure_cards_repository, session):
     ).all()
     
     assert len(shown_cards_player1) == 3
+    
+
+@pytest.mark.integration_test
+def test_grab_figure_cards_none_needed(figure_cards_repository, session):
+    
+    game = Game(name="Test Game", max_players=4, min_players=2)
+    session.add(game)
+    session.commit()
+    
+    game_state = GameState(game_id = game.id, state=StateEnum.PLAYING)
+    session.add(game_state)
+    session.commit()
+    
+    player1 = Player(name="Player1", game_id=game.id, game_state_id=game_state.id, host=True, winner=False)
+    session.add_all([player1])
+    session.commit()
+    
+    session.add_all([
+        FigureCard(player_id=player1.id, game_id=game.id, show=True, type=typeEnum.TYPE_1),
+        FigureCard(player_id=player1.id, game_id=game.id, show=True, type=typeEnum.TYPE_1),
+        FigureCard(player_id=player1.id, game_id=game.id, show=True, type=typeEnum.TYPE_1),
+        FigureCard(player_id=player1.id, game_id=game.id, show=False, type=typeEnum.TYPE_1),
+        FigureCard(player_id=player1.id, game_id=game.id, show=False, type=typeEnum.TYPE_1),
+        FigureCard(player_id=player1.id, game_id=game.id, show=False, type=typeEnum.TYPE_1),
+        FigureCard(player_id=player1.id, game_id=game.id, show=False, type=typeEnum.TYPE_1),
+        FigureCard(player_id=player1.id, game_id=game.id, show=False, type=typeEnum.TYPE_1),
+    ])
+    session.commit()
+    
+    figure_cards_repository.grab_figure_cards(player1.id, game.id, session)
+
+    shown_cards_player1 = session.query(FigureCard).filter(
+        FigureCard.player_id == player1.id,
+        FigureCard.game_id == game.id,
+        FigureCard.show == True
+    ).all()
+    #Reviso que no el jugador no obtuvo mas cartas del mazo
+    assert len(shown_cards_player1) == 3
+    
+@pytest.mark.integration_test
+def test_grab_figure_cards_no_player(figure_cards_repository, session):
+    
+    game = Game(name="Test Game", max_players=4, min_players=2)
+    session.add(game)
+    session.commit()
+    
+    game_state = GameState(game_id = game.id, state=StateEnum.PLAYING)
+    session.add(game_state)
+    session.commit()
+    
+    #le paso un id de un jugador que no existe en la db de test
+    player_id = 7643868
+    
+    with pytest.raises(HTTPException) as exc_info:
+        figure_cards_repository.grab_figure_cards(player_id, game.id, session)
+    
+    # Verificar que la excepción HTTP tiene el código de estado correcto
+    assert exc_info.value.status_code == 404
+    assert exc_info.value.detail == "Player not found in the game"
+
+@pytest.mark.integration_test
+def test_grab_figure_cards_no_game(figure_cards_repository, session):
+    
+    #le paso un id de un jugador que no existe en la db de test
+    game_id = 843565
+    #le paso un id de un jugador cq
+    player_id = 6
+    
+    with pytest.raises(HTTPException) as exc_info:
+        figure_cards_repository.grab_figure_cards(player_id, game_id, session)
+    
+    # Verificar que la excepción HTTP tiene el código de estado correcto
+    assert exc_info.value.status_code == 404
+    assert exc_info.value.detail == "No game found"
--- tests/test_movement_cards_repository.py
+++ tests/test_movement_cards_repository.py
@@ -1,13 +1,13 @@
 import pytest
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import NoResultFound
+
+from fastapi import HTTPException
+
 from movementCards.movement_cards_repository import MovementCardsRepository
-from board.models import  Board, Box
-from figureCards.models import FigureCard
 from game.models import Game
 from gameState.models import GameState, StateEnum
 from movementCards.models import MovementCard, typeEnum
-from figureCards.models import FigureCard
 from player.models import Player
 
 from database.db import engine
@@ -81,8 +81,8 @@ def test_grab_mov_cards(movement_cards_repository: MovementCardsRepository, sess
     session.add(game_state)
     session.commit()
     
-    player1 = Player(id=28, name="Player1", game_id=game.id, game_state_id=game_state.id, host=True, winner=False)
-    player2 = Player(id=29, name="Player2", game_id=game.id, game_state_id=game_state.id, host=False, winner=False)
+    player1 = Player(name="Player1", game_id=game.id, game_state_id=game_state.id, host=True, winner=False)
+    player2 = Player(name="Player2", game_id=game.id, game_state_id=game_state.id, host=False, winner=False)
     session.add_all([player1, player2])
     session.commit()
     
@@ -114,3 +114,137 @@ def test_grab_mov_cards(movement_cards_repository: MovementCardsRepository, sess
     
     assert len(shown_cards_player1) == 3
     assert len(shown_cards_player2) == 3
+    
+@pytest.mark.integration_test
+def test_grab_mov_cards_no_game(movement_cards_repository: MovementCardsRepository, session):
+    
+    game_id = 45363
+    player_id = 1
+    
+    with pytest.raises(HTTPException) as exc_info:
+        movement_cards_repository.grab_mov_cards(player_id, game_id, session)
+
+    # Verificar que la excepción HTTP tiene el código de estado correcto
+    assert exc_info.value.status_code == 404
+    assert exc_info.value.detail == "No game found"
+    
+@pytest.mark.integration_test
+def test_grab_mov_cards_no_player(movement_cards_repository: MovementCardsRepository, session):
+    
+    game = Game(name="Test Game", max_players=4, min_players=2)
+    session.add(game)
+    session.commit()
+    
+    game_state = GameState(game_id = game.id, state=StateEnum.PLAYING)
+    session.add(game_state)
+    session.commit()
+    
+    player1 = Player(name="Player1", game_id=game.id, game_state_id=game_state.id, host=True, winner=False)
+    player2 = Player(name="Player2", game_id=game.id, game_state_id=game_state.id, host=False, winner=False)
+    session.add_all([player1, player2])
+    session.commit()
+    
+    player_id = 123141
+    
+    with pytest.raises(HTTPException) as exc_info:
+        movement_cards_repository.grab_mov_cards(player_id, game.id, session)
+
+    # Verificar que la excepción HTTP tiene el código de estado correcto
+    assert exc_info.value.status_code == 404
+    assert exc_info.value.detail == "Player not found"
+    
+    
+@pytest.mark.integration_test
+def test_grab_mov_cards_and_reshuffle(movement_cards_repository: MovementCardsRepository, session):
+    
+    game = Game(name="Test Game", max_players=4, min_players=2)
+    session.add(game)
+    session.commit()
+    
+    game_state = GameState(game_id = game.id, state=StateEnum.PLAYING)
+    session.add(game_state)
+    session.commit()
+    
+    player1 = Player(name="Player1", game_id=game.id, game_state_id=game_state.id, host=True, winner=False)
+    player2 = Player(name="Player2", game_id=game.id, game_state_id=game_state.id, host=False, winner=False)
+    session.add_all([player1, player2])
+    session.commit()
+    
+    session.add_all([
+        MovementCard(player_id = player1.id ,game_id=game.id,type=typeEnum.DIAGONAL_CONT, description = '', used= False),
+        MovementCard(player_id = player1.id , game_id=game.id, type=typeEnum.EN_L_DER, description = '', used= False),
+        MovementCard(game_id=game.id, type=typeEnum.DIAGONAL_CONT, description = '', used= True),
+        MovementCard(player_id = player2.id , game_id=game.id, type=typeEnum.LINEAL_AL_LAT, description = '', used= False),
+        MovementCard(game_id=game.id, type=typeEnum.DIAGONAL_CONT, description = '', used= True),
+        MovementCard(game_id=game.id, type=typeEnum.DIAGONAL_CONT, description = '', used= True),
+        MovementCard(game_id=game.id, type=typeEnum.LINEAL_CONT, description = '', used= True),
+        MovementCard(game_id=game.id, type=typeEnum.DIAGONAL_CONT, description = '', used= True),
+        MovementCard(game_id=game.id, type=typeEnum.LINEAL_ESP, description = '', used= True)
+    ])
+    session.commit()
+    
+    movement_cards_repository.grab_mov_cards(player1.id, game.id, session)
+    movement_cards_repository.grab_mov_cards(player2.id, game.id, session)
+
+    shown_cards_player1 = session.query(MovementCard).filter(
+        MovementCard.player_id == player1.id,
+        MovementCard.game_id == game.id,
+    ).all()
+    
+    shown_cards_player2 = session.query(MovementCard).filter(
+        MovementCard.player_id == player2.id,
+        MovementCard.game_id == game.id,
+    ).all()
+    
+    assert len(shown_cards_player1) == 3
+    assert len(shown_cards_player2) == 3
+    
+@pytest.mark.integration_test
+def test_reshuffle_movement_deck(movement_cards_repository: MovementCardsRepository, session):
+    # Crear un juego y agregarlo a la sesión
+    game = Game(name="My Game", max_players=3, min_players=2)
+    session.add(game)
+    session.commit()
+    
+    game_state = GameState(game_id = game.id, state=StateEnum.PLAYING)
+    session.add(game_state)
+    session.commit()
+    
+    player1 = Player(name="Player1", game_id=game.id, game_state_id=game_state.id, host=True, winner=False)
+    player2 = Player(name="Player2", game_id=game.id, game_state_id=game_state.id, host=False, winner=False)
+    session.add_all([player1, player2])
+    session.commit()
+
+    # Agregar cartas de movimiento usadas y no usadas al juego
+    session.add_all([
+        MovementCard(game_id=game.id, type=typeEnum.DIAGONAL_CONT, description='', used=True),
+        MovementCard(game_id=game.id, type=typeEnum.EN_L_DER, description='', used=True),
+        MovementCard(game_id=game.id, type=typeEnum.LINEAL_AL_LAT, description='', used=False),
+        MovementCard(game_id=game.id, type=typeEnum.DIAGONAL_CONT, description='', used=True),
+        MovementCard(game_id=game.id, type=typeEnum.LINEAL_CONT, description='', used=True),
+        MovementCard(player_id = player1.id, game_id=game.id, type=typeEnum.LINEAL_CONT, description='', used=False),
+        MovementCard(player_id = player1.id, game_id=game.id, type=typeEnum.LINEAL_CONT, description='', used=False),
+        MovementCard(player_id = player2.id, game_id=game.id, type=typeEnum.LINEAL_CONT, description='', used=False),
+    ])
+    session.commit()
+
+    # Llamar al método reshuffle_movement_deck
+    movement_cards_repository.reshuffle_movement_deck(game.id, session)
+    
+    # Verificar que todas las cartas usadas ahora est
+    # án marcadas como no usadas
+    used_cards_after_reshuffle = session.query(MovementCard).filter(
+        MovementCard.game_id == game.id,
+        MovementCard.player_id.is_(None),
+        MovementCard.used == True
+    ).all()
+
+    assert len(used_cards_after_reshuffle) == 0
+
+    unused_cards_after_reshuffle = session.query(MovementCard).filter(
+        MovementCard.game_id == game.id,
+        MovementCard.player_id.is_(None),
+        MovementCard.used == False
+    ).all()
+    
+    assert len(unused_cards_after_reshuffle) == 5
