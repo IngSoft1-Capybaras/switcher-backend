80b1dbfb4c0814296ad8f1f8e25eeef327141698
Ignacio Gomez <57643368+NachoGz@users.noreply.github.com>
2024-10-09 13:09:00 -0300

    added undo_movement endpoint

--- movementCards/endpoints.py
+++ movementCards/endpoints.py
@@ -3,7 +3,11 @@ from sqlalchemy.orm import Session
 
 from database.db import get_db
 from .movement_cards_repository import MovementCardsRepository, get_movement_cards_repository
+from .movement_cards_logic import MovementCardLogic, get_mov_cards_logic
 from board.schemas import BoardPosition
+from board.board_repository import BoardRepository
+from partial_movement.partial_movement_repository import PartialMovementRepository, get_partial_movement_repository
+from connection_manager import manager
 
 movement_cards_router = APIRouter(
     prefix= "/deck/movement",
@@ -43,3 +47,23 @@ async def play_movement_card(game_id: int, player_id: int, card_id: int, pos_fro
     
     #Cambiar mensaje
     return {"message": "Play your cards right..."}
+
+
+@movement_cards_router.post("/undo_move")
+async def undo_movement(game_id: int, player_id: int, db: Session = Depends(get_db), 
+                        partial_mov_repo:  PartialMovementRepository = Depends(get_partial_movement_repository),
+                        board_repo: BoardRepository = Depends()):
+
+    last_movement = partial_mov_repo.undo_movement(db)
+
+    board_repo.swap_pieces(game_id, last_movement.pos_from_x, last_movement.pos_from_y,
+                           last_movement.pos_to_x, last_movement.pos_to_y, db)
+    
+    movement_update = {
+            "type": "MOVEMENT_UPDATE"
+    }
+    
+    await manager.broadcast(movement_update)
+
+    return {"message": "The movement was undone successfully"}
+    
