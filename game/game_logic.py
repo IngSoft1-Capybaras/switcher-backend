c4578e162a11632728cae853fa9fd333764a17c2
Ignacio Gomez <57643368+NachoGz@users.noreply.github.com>
2024-10-09 21:05:22 -0300

    added figure_card repo

--- game/game_logic.py
+++ game/game_logic.py
@@ -2,34 +2,42 @@ from sqlalchemy.orm import Session
 from fastapi import HTTPException, Depends
 from sqlalchemy.exc import NoResultFound
 from .models import Game
-from game.game_repository import GameRepository
+from game.game_repository import GameRepository, get_game_repository
 from gameState.game_state_repository import GameStateRepository
 from gameState.models import StateEnum
 from player.models import Player
 from player.player_repository import PlayerRepository
+from figureCards.figure_cards_repository import FigureCardsRepository, get_figure_cards_repository
 from connection_manager import manager
 
 
-def get_game_logic(game_repo: GameRepository = Depends(), game_state_repo : GameStateRepository = Depends(), player_repo : PlayerRepository = Depends()):
-    return GameLogic(game_repo,game_state_repo, player_repo)
+def get_game_logic(game_repo: GameRepository = Depends(get_game_repository), 
+                   game_state_repo : GameStateRepository = Depends(), player_repo : PlayerRepository = Depends(), 
+                   figure_cards_repo: FigureCardsRepository = Depends(get_figure_cards_repository)):
+    
+    return GameLogic(game_repo,game_state_repo, player_repo, figure_cards_repo)
 
 class GameLogic:
-    def __init__(self, game_repository: GameRepository, game_state_repository: GameStateRepository, player_repository: PlayerRepository):
+    def __init__(self, game_repository: GameRepository, game_state_repository: GameStateRepository, 
+                 player_repository: PlayerRepository, figure_cards_repo: FigureCardsRepository):
         self.game_repository = game_repository
         self.game_state_repo = game_state_repository
         self.player_repo = player_repository
+        self.figure_cards_repo = figure_cards_repo
 
 
     async def check_win_condition(self, game_id: int, db: Session):
+
         # chequeo si queda solo uno
-        
         players_left = self.player_repo.get_players_in_game(game_id, db)
         
         if len(players_left) == 1:
             await self.handle_win(game_id, players_left[0].id, db)
             return True
-        
+  
         return False
+    
+
         # aca irian el resto de las condiciones que se veran en otros sprints
 
 
