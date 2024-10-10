5d95bfdee7c7443787dd9ea0e64e91c3f14c26e3
Hakush <facundo.sharry@gmail.com>
2024-10-09 22:09:37 -0300

    SCRUM-74 play_card endpoint added (using logic and not repo)

--- figureCards/endpoints.py
+++ figureCards/endpoints.py
@@ -1,12 +1,20 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from database.db import get_db
-from .figure_cards_repository import FigureCardsRepository, get_figure_cards_repository
 
-figure_cards_router = APIRouter()
+from database.db import get_db
+
+from .figure_cards_logic import FigureCardsLogic, get_fig_cards_logic
+from .figure_cards_repository import (FigureCardsRepository,
+                                      get_figure_cards_repository)
+from .schemas import PlayFigureCardInput
+
+figure_cards_router = APIRouter(
+    prefix="/deck/figure",
+    tags=['Figure Cards']
+)
 
 # Obtener todas las cartas de figura
-@figure_cards_router.get("/deck/figure/{game_id}/{player_id}")
+@figure_cards_router.get("/{game_id}/{player_id}")
 async def get_figure_cards(game_id: int, player_id: int, 
                            db: Session = Depends(get_db), 
                            repo: FigureCardsRepository = Depends(get_figure_cards_repository)):
@@ -15,9 +23,15 @@ async def get_figure_cards(game_id: int, player_id: int,
 
 
 # Obtener una carta de figura especifica
-@figure_cards_router.get("/deck/figure/{game_id}/{player_id}/{card_id}")
+@figure_cards_router.get("/{game_id}/{player_id}/{card_id}")
 async def get_figure_card_by_id(game_id: int, player_id: int, 
                                 card_id: int, db: Session = Depends(get_db), 
                                 repo: FigureCardsRepository = Depends(get_figure_cards_repository)):
     
     return repo.get_figure_card_by_id(game_id, player_id, card_id, db)
+
+# Jugar una carta de figura
+@figure_cards_router.post("/play_card")
+async def play_figure_card(figureInfo: PlayFigureCardInput, logic: FigureCardsLogic = Depends(get_fig_cards_logic), db: Session = Depends(get_db)):
+
+    return logic.play_figure_card(figureInfo, db)
