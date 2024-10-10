32b3212c908140c1050fd9092d041258e48f33e2
Ignacio Gomez <57643368+NachoGz@users.noreply.github.com>
2024-10-10 00:37:57 -0300

    added discard_figure_card endpoint for testing

--- figureCards/endpoints.py
+++ figureCards/endpoints.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from database.db import get_db
 from .figure_cards_repository import FigureCardsRepository, get_figure_cards_repository
+from game.game_logic import GameLogic, get_game_logic
 
 figure_cards_router = APIRouter()
 
@@ -21,3 +22,15 @@ async def get_figure_card_by_id(game_id: int, player_id: int,
                                 repo: FigureCardsRepository = Depends(get_figure_cards_repository)):
     
     return repo.get_figure_card_by_id(game_id, player_id, card_id, db)
+
+
+# Jugar una carta de figura
+@figure_cards_router.post("/deck/figure/discard_card")
+async def discard_figure_card(game_id: int, player_id: int, 
+                              card_id: int, db: Session = Depends(get_db), 
+                              repo: FigureCardsRepository = Depends(get_figure_cards_repository), 
+                              game_logic: GameLogic = Depends(get_game_logic)):
+    
+    res = repo.discard_figure_card(game_id, player_id, card_id, db)
+
+    await game_logic.check_win_condition(game_id, player_id, db)
