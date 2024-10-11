84e85dd797e9260001edff7dc526c5293d7bb966
rxciog <rocio.guadalupe.gomez@mi.unc.edu.ar>
2024-10-11 11:47:19 -0300

    SCRUM-76 añadir verificaciones al crear el movimiento parcial

--- partial_movement/partial_movement_repository.py
+++ partial_movement/partial_movement_repository.py
@@ -5,12 +5,33 @@ from sqlalchemy.exc import IntegrityError
 from .models import PartialMovements
 
 from board.schemas import BoardPosition
+from game.models import Game
+from player.models import Player
+from movementCards.models import MovementCard
 
 
 
 class PartialMovementRepository:
     
     def create_partial_movement(self, game_id: int, player_id: int, card_id: int, pos_from: BoardPosition , pos_to: BoardPosition, db: Session):
+        # Verificamos exist el juego
+        game = db.query(Game).filter(Game.id == game_id).first()
+        if not game:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Game not found")
+        
+        # verificamos si existe el jugador en el juego
+        player = db.query(Player).filter(Player.id == player_id, Player.game_id == game_id).first()
+        if not player:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Player not found in the specified game")
+        
+        # cerificamos si existe la carta en la mano del jugador en el juego
+        card = db.query(MovementCard).filter(MovementCard.id == card_id, 
+                                             MovementCard.player_id == player_id, 
+                                             MovementCard.game_id == game_id
+                                            ).first()
+        if not card:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Card not found for the specified player and game")
+        
         partial_movement = PartialMovements(
             game_id=game_id,
             player_id=player_id,
@@ -21,11 +42,7 @@ class PartialMovementRepository:
             pos_to_y=pos_to.pos[1]
         )
         
-        try: 
-            db.add(partial_movement)
-            db.commit()
-        except IntegrityError:
-            #si alguna de las FK falla (por ejemplo no existen en la DB, enviamos un mensaje de error)
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Integrity error while creating partial movement: check foreign key values")
+        db.add(partial_movement)
+        db.commit()
         
         
