44e8b4f2ac29c6763068fe7e7d0dfadd14655792
Hakush <facundo.sharry@gmail.com>
2024-10-12 20:33:21 -0300

    SCRUM-91 added logic to get formed figures before getting the configured board and then merging them

--- board/endpoints.py
+++ board/endpoints.py
@@ -1,10 +1,19 @@
 import random
+
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
+
 from database.db import get_db
-from .models import Board, Box
-from .schemas import BoxOut
+from figureCards.figure_cards_logic import (FigureCardsLogic,
+                                            get_fig_cards_logic)
+from figureCards.figure_cards_repository import (FigureCardsRepository,
+                                                 get_figure_cards_repository)
+from player.player_repository import PlayerRepository
+
+from .board_logic import BoardLogic
 from .board_repository import BoardRepository
+from .models import Board, Box
+from .schemas import BoardAndBoxesOut, BoxOut
 
 board_router = APIRouter(
     prefix="/board",
@@ -13,4 +22,12 @@ board_router = APIRouter(
 
 @board_router.get("/{game_id}")
 async def get_board(game_id: int, db: Session = Depends(get_db), repo: BoardRepository = Depends()):
-    return repo.get_configured_board(game_id, db)
+    # obtener figuras formadas
+    figRepo = FigureCardsRepository()
+    playerRepo = PlayerRepository()
+    figLogic = FigureCardsLogic(figRepo, playerRepo)
+    formed_figures = figLogic.get_formed_figures(game_id, db)
+    result = repo.get_configured_board(game_id, db)
+    result_dict = result.dict()
+    result_dict["formed_figures"] = formed_figures
+    return BoardAndBoxesOut(**result_dict)
