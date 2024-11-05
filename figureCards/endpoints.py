5a2f56219ae50b8cafef47dc643253bf10b96fed
Ignacio Gomez <57643368+NachoGz@users.noreply.github.com>
2024-11-05 17:49:51 -0300

    SCRUM-144 add block_card endpoint

--- figureCards/endpoints.py
+++ figureCards/endpoints.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from database.db import get_db
 from .figure_cards_repository import FigureCardsRepository, get_figure_cards_repository
 from .figure_cards_logic import FigureCardsLogic, get_fig_cards_logic
 from game.game_logic import GameLogic, get_game_logic
 from .schemas import PlayFigureCardInput
+from connection_manager import manager
 
 figure_cards_router = APIRouter()
 
@@ -31,3 +32,13 @@ async def get_figure_card_by_id(game_id: int, player_id: int,
 async def play_figure_card(figureInfo: PlayFigureCardInput, logic: FigureCardsLogic = Depends(get_fig_cards_logic), db: Session = Depends(get_db)):
 
     return await logic.play_figure_card(figureInfo, db)
+
+
+# bloquear una carta de figura
+@figure_cards_router.post("/deck/figure/{game_id}/{figure_card_id}/block_card", status_code = status.HTTP_201_CREATED)
+async def play_figure_card(game_id: int, figure_card_id: int, repo: FigureCardsRepository = Depends(get_figure_cards_repository), db: Session = Depends(get_db)):
+
+    message = {"type": f"{game_id}:BLOCK_CARD"}
+    await manager.broadcast(message)
+
+    return repo.block_figure_card(figure_card_id, db)
