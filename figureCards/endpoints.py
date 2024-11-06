72cf9ea9a2e8c50a71f149b310219b1c7bc42766
Ignacio Gomez <57643368+NachoGz@users.noreply.github.com>
2024-11-06 16:31:40 -0300

    SCRUM-144 added player_id arg and validation

--- figureCards/endpoints.py
+++ figureCards/endpoints.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from database.db import get_db
 from .figure_cards_repository import FigureCardsRepository, get_figure_cards_repository
@@ -35,10 +35,14 @@ async def play_figure_card(figureInfo: PlayFigureCardInput, logic: FigureCardsLo
 
 
 # bloquear una carta de figura
-@figure_cards_router.post("/deck/figure/{game_id}/{figure_card_id}/block_card", status_code = status.HTTP_201_CREATED)
-async def block_figure_card(game_id: int, figure_card_id: int, repo: FigureCardsRepository = Depends(get_figure_cards_repository), db: Session = Depends(get_db)):
+@figure_cards_router.post("/deck/figure/{game_id}/{player_id}/{figure_card_id}/block_card", status_code = status.HTTP_201_CREATED)
+async def block_figure_card(game_id: int, player_id: int, figure_card_id: int, repo: FigureCardsRepository = Depends(get_figure_cards_repository), 
+                            logic: FigureCardsLogic = Depends(get_fig_cards_logic), db: Session = Depends(get_db)):
+    valid = logic.check_valid_block(game_id, player_id, figure_card_id, db)
+    if valid:
+        message = {"type": f"{game_id}:BLOCK_CARD"}
+        await manager.broadcast(message)
 
-    message = {"type": f"{game_id}:BLOCK_CARD"}
-    await manager.broadcast(message)
-
-    return repo.block_figure_card(game_id, figure_card_id, db)
+        return repo.block_figure_card(game_id, figure_card_id, db)
+    else:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid blocking")
