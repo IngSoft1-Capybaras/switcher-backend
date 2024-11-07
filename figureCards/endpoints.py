d0da9d4b6546734b3ce7ea7c942ee409714a7864
Ignacio Gomez <57643368+NachoGz@users.noreply.github.com>
2024-11-07 10:35:58 -0300

    SCRUM-144 changed post by query to body

--- figureCards/endpoints.py
+++ figureCards/endpoints.py
@@ -4,7 +4,7 @@ from database.db import get_db
 from .figure_cards_repository import FigureCardsRepository, get_figure_cards_repository
 from .figure_cards_logic import FigureCardsLogic, get_fig_cards_logic
 from game.game_logic import GameLogic, get_game_logic
-from .schemas import PlayFigureCardInput
+from .schemas import PlayFigureCardInput, BlockFigureCardInput
 from connection_manager import manager
 
 figure_cards_router = APIRouter()
@@ -35,8 +35,8 @@ async def play_figure_card(figureInfo: PlayFigureCardInput, logic: FigureCardsLo
 
 
 # bloquear una carta de figura
-@figure_cards_router.post("/deck/figure/{game_id}/{player_id}/{figure_card_id}/block_card", status_code = status.HTTP_200_OK)
-async def block_figure_card(game_id: int, player_id: int, figure_card_id: int, logic: FigureCardsLogic = Depends(get_fig_cards_logic), 
+@figure_cards_router.post("/deck/figure/block_card", status_code = status.HTTP_200_OK)
+async def block_figure_card(figureInfo: BlockFigureCardInput, logic: FigureCardsLogic = Depends(get_fig_cards_logic), 
                             db: Session = Depends(get_db)):
     
-    return await logic.block_figure_card(game_id, player_id, figure_card_id, db)
+    return await logic.block_figure_card(figureInfo, db)
